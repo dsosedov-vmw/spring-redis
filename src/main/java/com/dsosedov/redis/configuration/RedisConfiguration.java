a197f34dcccb30f91766eb116fb78cf4094a19d2
Dmitrii Sosedov <dsosedov@vmware.com>
2021-06-01 15:41:07 -0400

    Externalize Redis config

--- src/main/java/com/dsosedov/redis/configuration/RedisConfiguration.java
+++ src/main/java/com/dsosedov/redis/configuration/RedisConfiguration.java
@@ -8,18 +8,23 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
+import java.util.logging.Logger;
+
 @Configuration
 @EnableRedisRepositories
 public class RedisConfiguration {
 
-    @Value("${redis.host:127.0.0.1}")
+    private Logger logger = Logger.getLogger(RedisConfiguration.class.getName());
+
+    @Value("${redis.host}")
     private String host;
 
-    @Value("${redis.port:6379}")
+    @Value("${redis.port}")
     private Integer port;
 
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
+        logger.info("Trying to connect to Redis on " + host + ":" + port);
         RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host, port);
         return new JedisConnectionFactory(configuration);
     }
