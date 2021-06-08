43e3d4951426b654d77ff7962cfa8c24b507a8ee
Dmitrii Sosedov <dsosedov@vmware.com>
2021-06-08 15:24:28 -0400

    WIP

--- src/main/java/com/dsosedov/redis/configuration/ProdRedisConfiguration.java
+++ src/main/java/com/dsosedov/redis/configuration/ProdRedisConfiguration.java
@@ -1,5 +1,6 @@
 package com.dsosedov.redis.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
@@ -8,22 +9,20 @@ import org.springframework.data.redis.connection.RedisNode;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
 import java.util.Collection;
-import java.util.logging.Logger;
 
 @Profile("prod")
+@Slf4j
 public class ProdRedisConfiguration extends RedisConfigurationBase {
 
     @Value("${spring.redis.cluster.nodes}")
     private Collection<String> redisClusterNodes;
 
-    Logger logger = Logger.getLogger(ProdRedisConfiguration.class.getName());
-
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        logger.info("Connecting to Redis cluster...");
+        log.debug("Connecting to Redis cluster...");
         RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration(redisClusterNodes);
         for (RedisNode node : redisClusterConfiguration.getClusterNodes()) {
-            logger.info(node.getHost() + ":" + node.getPort());
+            log.debug(node.getHost() + ":" + node.getPort());
         }
         return new JedisConnectionFactory(redisClusterConfiguration);
     }
